879ac8e87b4deb09607e141d1e5a3498baf4dbea
Artur Zahorski <artur.zahorski@am.edu.pl>
2019-03-18 16:25:59 +0100

    Add react-router-dom, About page and navigation

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../css/Header.css';
 
 export default function Header() {
   return (
     <div className="header-cnt">
       <h1>TodoList</h1>
+      <div>
+        <Link className="nav-link" to="/">Home</Link> | <Link className="nav-link" to="/About">About</Link>
+      </div>
     </div>
   )
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../css/about.css';
+
+export default function About() {
+  return (
+    <div className="about-cnt">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+        Maecenas maximus nec velit id auctor. Nunc felis mi, 
+        rhoncus non ex sit amet, efficitur sollicitudin massa. 
+        Phasellus dictum libero lorem, ut consectetur dolor malesuada eget. 
+        Curabitur cursus maximus tellus, ut hendrerit ex condimentum eu. 
+        Nullam ullamcorper velit est, in hendrerit nisl laoreet non. 
+      </p>
+    </div>
+  )
+}
