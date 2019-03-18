3a94ae68d04d0a66e6d346da30d7c0c47c4dd932
Artur Zahorski <artur.zahorski@am.edu.pl>
2019-03-18 13:18:23 +0100

    Add Todos and TodoItem components and create PropTypes for them

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos';
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        compleated: false
+      },
+      {
+        id: 2,
+        title: 'Make a dinner',
+        compleated: false
+      },
+      {
+        id: 3,
+        title: 'Clean a room',
+        compleated: false
+      }
+    ]
+  }
+
   render() {
+  
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
---
+++ src/components/Todos.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+  render() {
+    
+    //Loop przez state w app.js
+    return this.props.todos.map((todo) => (
+        //przekazanie prop do todoitem.. przy stosowaniu map() należy podać unikatową wartość dla key. Najlepiej ID.
+        <TodoItem key={todo.id} todo={todo}/>
+    ));
+  }
+}
+
+
+//PropTypes - Walidacja wymaganych props-ów
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired
+}
+
+export default Todos;
